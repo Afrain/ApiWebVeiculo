61c01ca3b3f912fdb7abbff8bb8fc9062e9cda07
afrain <calixtoafrain@gmail.com>
2023-04-10 19:52:22 -0300

    Implementando a classe Veiculo e seus atributos

---
+++ ApiWebVeiculo/Models/Veiculo.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiWebVeiculo.Models
+{
+    public class Veiculo
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Marca { get; set; }
+        public int Ano { get; set; }
+        public decimal Preco { get; set; }
+        public string? Cor { get; set; }
+    }
+}
