b313d8d26c1a940408e68da1c1b6eceb863f11ff
afrain <calixtoafrain@gmail.com>
2023-04-10 20:27:22 -0300

    Implementando a classe VeiculoDBContext

---
+++ ApiWebVeiculo/Data/VeiculoDBContext.cs
@@ -0,0 +1,17 @@
+﻿using ApiWebVeiculo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiWebVeiculo.Data
+{
+    public class VeiculoDBContext : DbContext
+    {
+        public VeiculoDBContext(DbContextOptions<VeiculoDBContext> veiculoDbContext) : base(veiculoDbContext) { }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
