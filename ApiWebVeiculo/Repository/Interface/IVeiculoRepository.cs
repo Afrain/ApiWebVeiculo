65674312eedfdd907a587de8a04c63d5f79d5e90
afrain <calixtoafrain@gmail.com>
2023-04-10 20:38:14 -0300

    Implementando a interface VeiculoRepository

---
+++ ApiWebVeiculo/Repository/Interface/IVeiculoRepository.cs
@@ -0,0 +1,13 @@
+﻿using ApiWebVeiculo.Models;
+
+namespace ApiWebVeiculo.Repository.Interface
+{
+    public interface IVeiculoRepository
+    {
+        Task<List<Veiculo>> BuscarTodosVeiculos();
+        Task<Veiculo> BuscarVeiculoId(int id);
+        Task<Veiculo> SalvarVeiculo(Veiculo veiculo);
+        Task<Veiculo> AlterarVeiculo(Veiculo veiculo);
+        Task<bool> ExcluirVeiculo(int id);
+    }
+}
