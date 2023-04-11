4b5623f2e6cacd512c67d6ecb1f6a835342af6cd
afrain <calixtoafrain@gmail.com>
2023-04-10 21:45:12 -0300

    Criando o mapeamento da classe veiculoMap

---
+++ ApiWebVeiculo/Data/Map/VeiculoMap.cs
@@ -0,0 +1,19 @@
+﻿using ApiWebVeiculo.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ApiWebVeiculo.Data.Map
+{
+    public class VeiculoMap : IEntityTypeConfiguration<Veiculo>
+    {
+        public void Configure(EntityTypeBuilder<Veiculo> builder)
+        {
+            builder.HasKey(veiculo => veiculo.Id);
+            builder.Property(veiculo => veiculo.Nome).IsRequired().HasMaxLength(50);
+            builder.Property(veiculo => veiculo.Marca).IsRequired().HasMaxLength(50);
+            builder.Property(veiculo => veiculo.Ano).IsRequired();
+            builder.Property(veiculo => veiculo.Preco).IsRequired();
+            builder.Property(veiculo => veiculo.Cor).IsRequired().HasMaxLength(20);
+        }
+    }
+}
